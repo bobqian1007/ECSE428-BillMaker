d0e5100d2f62c37a3b87e850c22de43b25955587
Bobqian <bobqian1007@outlook.com>
2021-01-27 12:29:52 +0800

    Adding Gradle wrapper

--- billmaker-backend/src/main/java/com/ecse428/billmaker/BillMakerBackendApplication.java
+++ billmaker-backend/src/main/java/com/ecse428/billmaker/BillMakerBackendApplication.java
@@ -2,7 +2,10 @@ package com.ecse428.billmaker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RestController
 @SpringBootApplication
 public class BillMakerBackendApplication {
 
