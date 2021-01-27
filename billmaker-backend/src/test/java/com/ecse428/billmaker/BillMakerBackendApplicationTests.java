3b6cd83b5e8373cba9c228ce1354fbe694022cc2
Bobqian <bobqian1007@outlook.com>
2021-01-27 13:58:50 +0800

    change spring type

--- billmaker-backend/src/test/java/com/ecse428/billmaker/BillMakerBackendApplicationTests.java
+++ billmaker-backend/src/test/java/com/ecse428/billmaker/BillMakerBackendApplicationTests.java
@@ -1,7 +1,9 @@
 package com.ecse428.billmaker;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 class BillMakerBackendApplicationTests {
