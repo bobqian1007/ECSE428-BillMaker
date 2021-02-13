dece451d4d85c6436c96b21c41e44bfc8e621afa
liuzheyu1998 <zheyu.liu@mail.mcgill.ca>
2021-02-13 23:57:07 +0800

    Edit Bill test for error flow

--- billmaker-backend/src/test/java/Step/Edit_Bill.java
+++ billmaker-backend/src/test/java/Step/Edit_Bill.java
@@ -13,10 +13,12 @@ public class Edit_Bill {
     Expense e;
     int amount = 10;
     ExpenseService es = new ExpenseService();
+    String error = "";
 
     @Given("user is logged into the Bill management system as an indivudual user")
     public void user_is_logged_into_the_bill_management_system_as_an_indivudual_user() {
-        // Write code here that turns the phrase above into concrete actions
+        error ="";
+        amount = 10;
 
     }
 
@@ -44,17 +46,32 @@ public class Edit_Bill {
 
     @When("user change the amount of the bill to a same amount")
     public void user_change_the_amount_of_the_bill_to_a_same_amount() {
-        e.setAmount(e.getAmount());
+        try
+        {
+            es.editBill(e, 10);
+        }
+        catch(IllegalArgumentException re)
+        {
+            error = re.getMessage();
+        }
     }
 
     @When("user change the amount of the bill to a negative amount")
     public void user_change_the_amount_of_the_bill_to_a_negative_amount() {
-
+        try
+        {
+            es.editBill(e, -10);
+        }
+        catch(IllegalArgumentException re)
+        {
+            error = re.getMessage();
+        }
     }
 
     @Then("the system notifies the user the amount should not be positive")
     public void the_system_notifies_the_user_the_amount_should_not_be_positive() {
-
+        assertEquals(error, "Bill amount cannot be 0!");
+        error ="";
     }
 
 }
