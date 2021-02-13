3fe332c0d7c5f4ffb0f67bb697276a9350252119
Rintaro Nomura <nswe.nrin@gmail.com>
2021-02-13 17:59:54 -0500

    rename the package .service for good

--- billmaker-backend/src/main/java/com/ecse428/billmaker/controller/BillMakerController.java
+++ billmaker-backend/src/main/java/com/ecse428/billmaker/controller/BillMakerController.java
@@ -2,8 +2,10 @@ package com.ecse428.billmaker.controller;
 
 import com.ecse428.billmaker.dto.SupervisorUserDto;
 import com.ecse428.billmaker.model.SupervisorUser;
+import com.ecse428.billmaker.model.User;
 import com.ecse428.billmaker.service.BillMakerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,5 +33,20 @@ public class BillMakerController {
         }
 
         return new SupervisorUserDto(su.getUsername(), su.getPassword(), su.getEmail());
+    }
+
+
+    /**
+     * User Login Related
+     * - rn
+     */
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/userList")
+    public String getUserList(Model model) {
+        List<User> userList = userService.selectMany();
+        model.addAttribute("userList", userList);
+        return "userList";
     }
 }
