8086fc127dba80e3e3dd76504043c2205f4bf4bf
OldPineapple <hanwen.wang@mail.mcgill.ca>
2021-02-13 01:31:21 -0500

    Deleted TransactionRepository.java, which I created but never used.

--- billmaker-backend/src/main/java/com/ecse428/billmaker/dao/TransactionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecse428.billmaker.dao;
-
-import com.ecse428.billmaker.model.Transaction;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TransactionRepository extends CrudRepository<Transaction, String> {
-}
